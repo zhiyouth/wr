924ac80aeabadfa9378a56885a3f5d949f108d3c
zhiyouth <13243071208@163.com>
2020-03-29 23:00:07 +0800

    feat: add wr component

---
+++ src/common-components/Tab.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wr-tab">
+    <ul class="wr-tab-header">
+        <li :class="'wr-tab-header-tab tab-' + index" v-for="(item, index) in data">
+            
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>/*  */
+export default {
+  name: 'HelloWorld',
+  props: ['data', 'renderContent'],
+  data () {
+      return {};
+  },
+  created() {
+      console.log('-----------------------------------', this.data)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wr-tab {
+    &-header {
+        
+    }
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="index">
+        <Tab :data="data"/>
+        Index
+    </div>
+</template>
+
+<script>
+import Tab from '../common-components/Tab';
+export default {
+    name: 'Index',
+    components: {
+        Tab
+    },  
+    data () {
+        return {
+            data: [{}, {}, {}]
+        }
+    },
+    beforeCreate () {
+        console.log('beforeCreate')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
